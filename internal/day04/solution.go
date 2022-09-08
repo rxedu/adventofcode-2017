b02cbf9b0368da5c7b0afe610913691dfaa2f649
Evan Sosenko <razorx@evansosenko.com>
2022-09-08 15:24:24 -0500

    Solve day 4 part 2

--- internal/day04/solution.go
+++ internal/day04/solution.go
@@ -1,6 +1,7 @@
 package day04
 
 import (
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -24,13 +25,34 @@ func solvePartOne(input []string) int {
 }
 
 func solvePartTwo(input []string) int {
-	return 0
+	var count int
+	for _, v := range input {
+		if isValidPartTwoPassphrase(v) {
+			count++
+		}
+	}
+	return count
 }
 
 func isValidPartOnePassphrase(pass string) bool {
 	words := strings.Split(pass, " ")
 	m := make(map[string]int, len(words))
 	for _, v := range words {
+		m[v]++
+		if m[v] > 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func isValidPartTwoPassphrase(pass string) bool {
+	words := strings.Split(pass, " ")
+	m := make(map[string]int, len(words))
+	for _, v := range words {
+		k := strings.Split(v, "")
+		sort.Strings(k)
+		v = strings.Join(k, "")
 		m[v]++
 		if m[v] > 1 {
 			return false
