279045716e0701ac97ad44dad354f0fabb8fb164
Evan Sosenko <razorx@evansosenko.com>
2022-09-07 23:05:50 -0500

    Solve day 3 part 1

--- internal/day03/solution.go
+++ internal/day03/solution.go
@@ -13,11 +13,77 @@ func SolvePartTwo(input string) string {
 }
 
 func solvePartOne(input int) int {
-	return 0
+	adr := spiral(input)
+	return abs(adr.x) + abs(adr.y)
 }
 
 func solvePartTwo(input int) int {
 	return 0
+}
+
+type address struct {
+	n int
+	z int
+	x int
+	y int
+}
+
+func spiral(to int) address {
+	adr := address{x: 0, y: 0, z: 1, n: 0}
+
+	if adr.z == to {
+		return adr
+	}
+
+	for adr.z < to {
+		adr.x++
+		adr.z++
+		adr.n++
+		if adr.z == to {
+			return adr
+		}
+
+		for adr.y < adr.n {
+			adr.y++
+			adr.z++
+			if adr.z == to {
+				return adr
+			}
+		}
+
+		for adr.x > -adr.n {
+			adr.x--
+			adr.z++
+			if adr.z == to {
+				return adr
+			}
+		}
+
+		for adr.y > -adr.n {
+			adr.y--
+			adr.z++
+			if adr.z == to {
+				return adr
+			}
+		}
+
+		for adr.x < adr.n {
+			adr.x++
+			adr.z++
+			if adr.z == to {
+				return adr
+			}
+		}
+	}
+
+	return adr
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
 
 func parse(input string) int {
@@ -30,5 +96,4 @@ func parse(input string) int {
 
 func serialize(output int) string {
 	return strconv.Itoa(output)
-
 }
