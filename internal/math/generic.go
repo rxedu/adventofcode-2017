708da6104b084782e25ce36fa7869d38efc18340
Evan Sosenko <razorx@evansosenko.com>
2022-09-08 16:15:02 -0500

    Add math pkg

---
+++ internal/math/generic.go
@@ -0,0 +1,15 @@
+package math
+
+func Min[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max[T constraints.Ordered](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
