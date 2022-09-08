1e9e3fa956b81eb01291845942d190d52960861e
Evan Sosenko <razorx@evansosenko.com>
2022-09-08 15:00:38 -0500

    Setup day 4

--- days.go
+++ days.go
@@ -4,6 +4,7 @@ import (
 	"github.com/rxedu/adventofcode-2017-go/internal/day01"
 	"github.com/rxedu/adventofcode-2017-go/internal/day02"
 	"github.com/rxedu/adventofcode-2017-go/internal/day03"
+	"github.com/rxedu/adventofcode-2017-go/internal/day04"
 )
 
 var solversPartOne []Solver
@@ -14,11 +15,13 @@ func initSolvers() {
 		day01.SolvePartOne,
 		day02.SolvePartOne,
 		day03.SolvePartOne,
+		day04.SolvePartOne,
 	}
 
 	solversPartTwo = []Solver{
 		day01.SolvePartTwo,
 		day02.SolvePartTwo,
 		day03.SolvePartTwo,
+		day04.SolvePartTwo,
 	}
 }
---
+++ internal/day04/solution.go
@@ -0,0 +1,30 @@
+package day04
+
+import (
+	"strconv"
+	"strings"
+)
+
+func SolvePartOne(input string) string {
+	return serialize(solvePartOne(parse(input)))
+}
+
+func SolvePartTwo(input string) string {
+	return serialize(solvePartTwo(parse(input)))
+}
+
+func solvePartOne(input []string) int {
+	return 0
+}
+
+func solvePartTwo(input []string) int {
+	return 0
+}
+
+func parse(input string) []string {
+	return strings.Split(input, "\n")
+}
+
+func serialize(output int) string {
+	return strconv.Itoa(output)
+}
