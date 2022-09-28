10de59c43e3468fa6ccc314f16a47c8ac3cde10a
Evan Sosenko <razorx@evansosenko.com>
2022-09-27 22:44:08 -0700

    Add makeGrid function

--- internal/day14/solution.go
+++ internal/day14/solution.go
@@ -17,25 +17,34 @@ func SolvePartTwo(input string) string {
 }
 
 func solvePartOne(input string) int {
-	size := 128
-	count := 0
-	for i := 0; i < size; i++ {
-		h := day10.KnotHash(fmt.Sprintf("%s-%d", input, i))
-		for _, v := range strings.Split(h.Bin(), "") {
-			w, err := strconv.Atoi(v)
-			if err != nil {
-				panic(err)
-			}
-			if w == 1 {
-				count++
-			}
-		}
-	}
+	_, count := makeGrid(input)
 	return count
 }
 
 func solvePartTwo(input string) int {
 	return 0
+}
+
+func makeGrid(input string) ([][]int, int) {
+	size := 128
+	count := 0
+	grid := make([][]int, size)
+	for i := 0; i < size; i++ {
+		h := day10.KnotHash(fmt.Sprintf("%s-%d", input, i))
+		row := strings.Split(h.Bin(), "")
+		grid[i] = make([]int, len(row))
+		for j, v := range row {
+			w, err := strconv.Atoi(v)
+			if err != nil {
+				panic(err)
+			}
+			grid[i][j] = w
+			if w == 1 {
+				count++
+			}
+		}
+	}
+	return grid, count
 }
 
 func serialize(output int) string {
